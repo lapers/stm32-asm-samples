f1562f49f953d99167e6d22719463be8e6066c72
root <root@lapbook.lapers.net>
2020-04-05 23:17:47 +0300

    Added timer example

--- stm32f103/timer/main.s
+++ stm32f103/timer/main.s
@@ -16,57 +16,52 @@ b   start
 
 start:
   mov32 r1, 0x40021018  @ RCC_APB2ENR address
-  mov32 r0, 0x15        @ enable GPIOs & AFIO
+  mov32 r0, 0x11        @ enable GPIOs & AFIO
   str   r0, [r1]
 
   mov32 r1, 0x40011004  @ GPIOC_CRH address
   mov32 r0, 0x44344444  @ set C13 as 50MHz push-pull output
   str   r0, [r1]
 
-  mov32 r1, 0x40010800  @ GPIOA_CRL address
-  mov32 r0, 0x44444448  @ PA0 as input
-  str   r0, [r1]
-
-  mov32 r1, 0x4001080C  @ GPIOA_ODR address
-  mov32 r0, 0x00000001  @ PA0 pull-up
-  str   r0, [r1]
-
 .include "sysclk.inc"
 
-  @ configure EXTI0
-  mov32 r1, 0x40010414  @ EXTI_PR address
-  mov32 r0, 0x00000001  @ EXTI0 flag drop
+  @ configure TIM3
+  mov32 r1, 0x4002101C  @ RCC_APB1ENR address
+  mov32 r0, 0x2         @ enable TIM3
   str   r0, [r1]
 
-  mov32 r1, 0x40010400  @ EXTI_IMR address
-  mov32 r0, 0x00000001  @ EXTI0 enable
+  mov32 r1, 0x40000428  @ TIM3_PSC address
+  mov32 r0, 7199        @ prescaler
   str   r0, [r1]
 
-  mov32 r1, 0x40010408  @ EXTI_RTSR address
-  mov32 r0, 0x00000001  @ EXTI0 trigger on rising edge
+  mov32 r1, 0x4000042C  @ TIM3_ARR address
+  mov32 r0, 9999        @ counter
   str   r0, [r1]
 
-  mov32 r1, 0x4001040C  @ EXTI_FTSR address
-  mov32 r0, 0x00000001  @ EXTI0 trigger on falling edge
+  mov32 r1, 0x4000040C  @ TIM3_DIER address
+  mov32 r0, 0x1         @ update interrupt enable
   str   r0, [r1]
 
-  mov32 r1, 0x40010008  @ AFIO_EXTICR1 address
-  mov32 r0, 0x00000000  @ EXTI0 select PA0
+
+
+  mov32 r1, 0x40000400  @ TIM3_CR1 address
+  mov32 r0, 0x1         @ enable
   str   r0, [r1]
 
   @ configure NVIC
   mov32 r1, 0xE000E100  @ NVIC_ISER0 address
-  mov32 r0, 0b01000000  @ EXTI0 enable
+  mov32 r0, 0x20000000  @ TIM3 enable
   str   r0, [r1]
 
 end:
     b   end
 
-nvic_exti0:
+nvic_tim3:
   push  { lr }
 
-  mov32 r1, 0x40010414  @ EXTI_PR address
-  mov32 r0, 0x00000001  @ EXTI0 flag drop
+  mov32 r1, 0x40000410  @ TIM3_SR address
+  ldr   r0, [r1]
+  eor   r0, 0x1         @ update interrupt flag drop
   str   r0, [r1]
 
   mov32 r1, 0x4001100C  @ GPIOC_CRH address
